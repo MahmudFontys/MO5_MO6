9dd5247e7f67abe4b2692db22d683d20eed3b81f
daanpietje <57893493+daanpietje@users.noreply.github.com>
2025-06-11 12:10:19 +0200

    started with block diagram

---
+++ MO5/Self_driving_car/Self_driving_car.srcs/sources_1/new/Speed_calc.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity speedcalc is
+    Port (
+        time_in     : in  STD_LOGIC_VECTOR(31 downto 0);
+        speed       : out STD_LOGIC_VECTOR(31 downto 0)
+    );
+end speedcalc;
+
+architecture Behav of speedcalc is
+signal speed_calc     : unsigned(31 downto 0) := (others => '0');
+constant SCALE_FACTOR : integer := 1021000; -- 0.2042 / 20 * 100.000.000
+begin
+
+process(time_in)
+begin
+    speed_calc <= to_unsigned(SCALE_FACTOR, 32) / unsigned(time_in);
+    speed <= std_logic_vector(speed_calc);
+end process;
+
+end Behav;
